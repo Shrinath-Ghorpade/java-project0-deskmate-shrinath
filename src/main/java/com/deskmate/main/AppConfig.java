fe805a8f55a9d3031094f27771e493a4753135b3
Shrinath-Ghorpade <shrinathghorpade07@gmail.com>
2026-02-18 14:56:28 +0530

    Add app config file

---
+++ src/main/java/com/deskmate/main/AppConfig.java
@@ -0,0 +1,29 @@
+package com.deskmate.main;
+
+import com.deskmate.main.controller.DeskController;
+import com.deskmate.main.dao.impl.JdbcDeskDao;
+import com.deskmate.main.service.DeskService;
+import com.deskmate.main.dao.*;
+
+public class AppConfig {
+
+    public DeskController deskController() {
+        DeskDao deskDao = new JdbcDeskDao();
+        DeskService deskService = new DeskService(deskDao);
+        return new DeskController(deskService);
+    }
+//
+//    public BookingController bookingController() {
+//        DeskDao deskDao = new JdbcDeskDao();
+//        BookingDao bookingDao = new JdbcBookingDao();
+//        PaymentDao paymentDao = new JdbcPaymentDao();
+//        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
+//        return new BookingController(bookingService);
+//    }
+//
+//    public ReportController reportController() {
+//        ReportDao reportDao = new JdbcReportDao();
+//        ReportService reportService = new ReportService(reportDao);
+//        return new ReportController(reportService);
+//    }
+}
