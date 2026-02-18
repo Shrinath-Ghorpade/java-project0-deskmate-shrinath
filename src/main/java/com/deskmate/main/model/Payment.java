6f1ded84bad11ed32023d9abfdfc279d718cc294
Shrinath-Ghorpade <shrinathghorpade07@gmail.com>
2026-02-18 22:23:01 +0530

    Add payment and booking dao implementation files

---
+++ src/main/java/com/deskmate/main/model/Payment.java
@@ -0,0 +1,37 @@
+package com.deskmate.main.model;
+
+import com.deskmate.main.enums.PaymentMode;
+import com.deskmate.main.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+
+    private long paymentId;
+    private long bookingId;
+    private PaymentMode mode;
+    private BigDecimal amount;
+    private PaymentStatus status;
+    private LocalDateTime paidAt;
+
+    public Payment(long paymentId,
+                   long bookingId,
+                   PaymentMode mode,
+                   BigDecimal amount,
+                   PaymentStatus status,
+                   LocalDateTime paidAt) {
+        this.paymentId = paymentId;
+        this.bookingId = bookingId;
+        this.mode = mode;
+        this.amount = amount;
+        this.status = status;
+        this.paidAt = paidAt;
+    }
+
+    public long getBookingId() { return bookingId; }
+    public PaymentMode getMode() { return mode; }
+    public BigDecimal getAmount() { return amount; }
+    public PaymentStatus getStatus() { return status; }
+    public LocalDateTime getPaidAt() { return paidAt; }
+}
